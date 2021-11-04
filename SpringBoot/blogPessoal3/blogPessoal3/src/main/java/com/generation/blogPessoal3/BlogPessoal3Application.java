cd6230512ca473b38c363b63abfd933f33de0716
GuilhermeCarluz <gclgcl6@gmail.com>
2021-11-04 10:07:14 -0300

    Atividade em Spring

---
+++ SpringBoot/blogPessoal3/blogPessoal3/src/main/java/com/generation/blogPessoal3/BlogPessoal3Application.java
@@ -0,0 +1,13 @@
+package com.generation.blogPessoal3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoal3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoal3Application.class, args);
+	}
+
+}
